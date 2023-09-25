a8c3797c71f80cf649e22d9fd315556d0d0ca05c
997sir <everyday1866@hotmail.com>
2023-09-25 15:19:24 +0000

    xdp

--- rust-examples/src/xdp.rs
+++ rust-examples/src/xdp.rs
@@ -9,10 +9,10 @@ fn main() {
     let builder = XdpSkelBuilder::default();
     let skel = builder.open().unwrap();
     let mut skel = skel.load().unwrap();
+    
     skel.attach().unwrap();
 
     print!("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` to see output of the BPF programs.\n");
-
 
     loop {
         eprint!(".");
