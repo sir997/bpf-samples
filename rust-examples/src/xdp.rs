33964d7ad409fd0841485460143ad6c7acbcb4fe
sir997 <67670807+sir997@users.noreply.github.com>
2023-09-26 23:08:35 +0800

    Update xdp.rs

--- rust-examples/src/xdp.rs
+++ rust-examples/src/xdp.rs
@@ -3,14 +3,17 @@ mod bpf;
 use std::{thread, time::Duration};
 
 use bpf::*;
-use libbpf_rs::skel::{OpenSkel, Skel, SkelBuilder};
+use libbpf_rs::skel::{OpenSkel, SkelBuilder};
 
 fn main() {
     let builder = XdpSkelBuilder::default();
     let skel = builder.open().unwrap();
     let mut skel = skel.load().unwrap();
-    
-    skel.attach().unwrap();
+
+    let link = skel.progs_mut().xdp_pass().attach_xdp(2).unwrap();
+    skel.links = XdpLinks {
+        xdp_pass: Some(link),
+    };
 
     print!("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` to see output of the BPF programs.\n");
 
